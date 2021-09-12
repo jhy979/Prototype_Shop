d5e370045c29d5c3011289625e9a8e8f2d971e13
jhy979 <jhy979@gmail.com>
2021-09-12 14:07:15 +0900

    장바구니 생성 - 선택 기능 - 추가 기능 - 삭제 기능 - 전부 삭제 기능 - 개별 계싼 - 전체 비용 계산 - 전체 삭제

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,11 +1,88 @@
+import {useMemo} from "react";
+import useActions from "../hooks/useActions";
+import useOrders from "../hooks/useOrders";
+import usePrototypes from "../hooks/usePrototypes";
+import Prototypes from "./Prototypes";
 export default function Orders (){
+  const orders = useOrders();
+  const prototypes = usePrototypes();
+  const {remove,removeAll} = useActions();
 
-  return (
-    <aside>
-      <div className="empty">
-        <div className="title">You don't have any orders.</div>
-        <div className="subtitle">Click on a + to add an order</div>
-      </div>
-    </aside>
-  )
+  const totalPrice = useMemo(()=>{
+    return orders.map((el)=>{
+      const {id,quantity} = el;
+      const prototype = prototypes.find(p=>p.id===id);
+      return prototype.price * quantity;
+    }).reduce((l,r)=>{
+      return l+r
+    },0,[orders,prototypes])
+  });
+
+  if (orders.length ===0){
+    return (
+      <aside>
+        <div className="empty">
+          <div className="title">You don't have any orders.</div>
+          <div className="subtitle">Click on a + to add an order</div>
+        </div>
+      </aside>
+    )
+  }
+  else {
+    return (
+      <aside>
+        <div className="order">
+          <div className="body">
+            {orders.map((el,idx)=>{
+              const {id} = el
+              const prototype = prototypes.find(p=>p.id === id)
+              const click = () =>{
+                remove(id);
+              }
+              return (
+                <div className="item" key={id}>
+                  <div className = "img">
+                    <video src={prototype.thumbnail} />
+                  </div>
+                  
+                  <div className="content">
+                  <p className="title">
+                    {prototype.title} x {el.quantity}
+                  </p>
+                  </div>
+
+                  <div className="action">
+                  <p className="price">
+                    $ {prototype.price * el.quantity}
+                  </p>
+                  <button className="btn btn--link">
+                    <i className="icon icon--cross" onClick={click}></i>
+                  </button>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+
+          <div className="total">
+            <hr/>
+            <div className="item">
+              <div className="content">Total</div>
+              <div className="action">
+                <div className="price"> $ {totalPrice} </div>
+              </div>
+              <button className="btn btn--link">
+                <i className="icon icon--delete" onClick={removeAll}/>
+              </button>
+            </div>
+            <button className="btn btn--secondary" style={{
+              width : '100%',
+              marginTop : 10
+            }}>Checkout</button>
+          </div>
+        </div>
+      </aside>
+    )
+  }
+
 }
